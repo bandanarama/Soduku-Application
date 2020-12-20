cf106326e6f138bbaa38b45ee194905d10839d2c
bandanarama <kvoon0@frostburg.edu>
2020-12-19 17:47:29 -0700

    user interface interface and project structure added

---
+++ src/src/Sudoku/SudokuApplication.java
@@ -0,0 +1,28 @@
+package Sudoku;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SudokuApplication extends Application {
+    private IUserInterfaceContract.View guiImplementation;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        guiImplementation = new userInterfaceContract(primaryStage);
+        try {
+            sudokuBuildLogic.build(guiImplementation);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/src/Sudoku/userinterface/IUserInterfaceContract.java
@@ -0,0 +1,13 @@
+package Sudoku.userinterface;
+
+public interface IUserInterfaceContract {
+    interface EventListener {
+        void onSudokuInput(int x, int y, int input);
+        void onDialogClick();
+    }
+
+    interface View {
+        void setListener(IUserInterfaceContract.EventListener listener);
+        void update
+    }
+}
